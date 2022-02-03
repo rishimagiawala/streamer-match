17a2a3edad25fdd1cc452147e4e7c786f6c9d4fa
rishimagiawala <rishimagiawala@gmail.com>
2022-02-03 00:22:30 +0000

    playing around

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { green, red } from '@mui/material/colors';
 
+const theme = createTheme({
+  
+});
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return( <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
+  
+  )
 }
 
 export default MyApp
